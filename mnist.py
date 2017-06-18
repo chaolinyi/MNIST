0ae57c7b4d40f8dcd5857e90eb13d66660e8cbc4
chaolinyi <huhaichaotd@139.com>
2017-06-18 22:54:23 +0800

    learn advanced mnist

--- mnist.py
+++ mnist.py
@@ -1,50 +1,79 @@
-import tensorflow as tf
 import input_data
+import tensorflow as tf
 
-class MNIST:
-  def __init__(self):
-    return
+# load MNIST data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-  def init_params(self, dir, x_pixels, y_pixels, classify_numbers, learning_rate):
-    '''
-    :param dir: mnist images directory
-    :param x_pixels: one image x pixels
-    :param y_pixels: one image y pixels
-    :param classify_numbers: classifying numbers
-    :param learning_rate: set training model regression rate
-    '''
-    # buffer mnist image datas
-    self.mnist = input_data.read_data_sets(dir, one_hot=True)
-    # create regression model
-    imag_pixels = x_pixels * y_pixels
-    self.x = tf.placeholder("float", [None, imag_pixels])
-    self.W = tf.Variable(tf.zeros([imag_pixels, classify_numbers]))
-    self.b = tf.Variable(tf.zeros([classify_numbers]))
-    self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)
-    # training model
-    self.y_ = tf.placeholder("float", [None,classify_numbers])
-    cross_entropy = -tf.reduce_sum(self.y_*tf.log(self.y))
-    self.train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
-    # init all variables
-    self.init = tf.initialize_all_variables()
-    # create session
-    self.sess = tf.Session()
+# run TensorFlow's Interactive Session
+sess = tf.InteractiveSession()
 
-  def run_learning_model(self):
-    self.sess.run(self.init)
+# calculation graphic
+# concrete Softmax regression model
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+# variables
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+sess.run(tf.initialize_all_variables())
+# predition loss function
+y = tf.nn.softmax(tf.matmul(x, W)+b)
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+# training model
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+for i in range(1000):
+  batch = mnist.train.next_batch(50)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+# evalue model
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print accuracy.eval(feed_dict={x:mnist.test.images, y_:mnist.test.labels})
 
-    for i in range(1000):
-      batch_xs, batch_ys = self.mnist.train.next_batch(100)
-      self.sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
-
-  def mnist_evaluate(self):
-    # evaluate my model
-    correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print self.sess.run(accuracy, feed_dict={self.x: self.mnist.test.images, self.y_: self.mnist.test.labels})
-
-if __name__ == "__main__":
-  ministInstance = MNIST()
-  ministInstance.init_params("MNIST_data/", 28, 28, 10, 0.01)
-  ministInstance.run_learning_model()
-  ministInstance.mnist_evaluate()
+# create many-level CN
+# initialize
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+# poolize
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1,2,2,1],strides=[1,2,2,1], padding='SAME')
+# first con-product
+W_conv1 = weight_variable([5,5,1,32])
+b_conv1 = bias_variable([32])
+x_image = tf.reshape(x,[-1,28,28,1])
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+# second con-product
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+# density connect
+W_fc1 = weight_variable([7*7*64,1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+# Dropout
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+# output
+W_fc2 = weight_variable([1024,10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1,W_fc2)+b_fc2)
+# traing model
+cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+  batch = mnist.train.next_batch(50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+    print "step %d, training accuracy %g" %(i,train_accuracy)
+  train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+print "test accuracy %g" %accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0})
